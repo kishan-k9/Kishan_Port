a4ae18c3953c6034f5d174124572e97c3b581293
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2023-10-30 22:50:02 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/kishan-k9/Portfolio"
+                href="https://github.com/kishan-k9/Kishan_Port"
                 target="_blank"
                 className="fork-btn-inner"
               >
