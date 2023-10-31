622fca60457a7d6afe4f610d436528ffc743c530
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2023-10-31 18:55:36 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,6 +9,22 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
+  const [heightOfMyScreen, setHeightOfMyScreen] = useState(window.innerHeight);
+
+  useEffect(() => {
+    // Update the screen height when the window is resized
+    const handleResize = () => {
+      setHeightOfMyScreen(window.innerHeight);
+    };
+
+    // Add an event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
@@ -19,20 +35,8 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
         <Row className="resume">
-          <div style={{height:'1430px', overflowY:'hidden'}}>
+        <div style={{ height: heightOfMyScreen > '824' ? '1430px' : '500px', overflowY: 'hidden' }}>
             <Document file={pdf} className="d-flex justify-content-center">
               <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
             </Document>
