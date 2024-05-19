6ea93954991fd291d24535518a2e9174bd0341d8
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2024-05-19 15:21:46 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
               isBlog={false}
               title="Handwritten Digit Recognition"
               description="The handwritten digitrecognition is the ability of computers to recognize human handwritten digits. It is a hard task forthe machine because handwritten digits are not perfect and can be made with many different flavors."
-              ghLink="https://github.com/kishan-k9/Projects/tree/main/Handwritten%20Digit%20Recognition%20Deep%20Learning%20Project"
+              ghLink="https://github.com/kishan-k9/Handwritten-Digit-Recognition-Deep-Learning-Project"
               // demoLink=""
             />
           </Col>
@@ -35,7 +35,7 @@ function Projects() {
               isBlog={false}
               title="Tomato Leaf Prediction"
               description="The Tomato Leaf Prediction Deep Learning Project is an innovative endeavour aimed at leveraging deep learning techniques to detect and classify diseases and anomalies in tomato plants by analysing images of their leaves."
-              ghLink="https://github.com/kishan-k9/Projects/tree/main/Extracting%20structured%20information%20from%20scanned%20invoices%20or%20invoice%20images"
+              ghLink="https://github.com/kishan-k9/Tomato-Leaf-Prediction/tree/main"
               // demoLink=""
             />
           </Col>
@@ -46,8 +46,8 @@ function Projects() {
               isBlog={false}
               title="Plant Leaf Disease Prediction"
               description="In this project, we've enhanced our Tomato Leaf Disease Detection system to cover 14-15 plant types with 39 disease classes. We trained our model using a training dataset containing 61,486 images. The model is used to predict the diseases of multiple plants by processing the input images."
-              // ghLink=""
-              demoLink=""
+              ghLink="https://github.com/kishan-k9/Plant_leaf_DD"
+             // demoLink=""
             />
           </Col>
         </Row>
